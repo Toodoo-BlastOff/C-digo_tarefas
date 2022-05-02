f61c9ff910bd0c7d69e768543bf9f26f9db9b9c7
Blast Off <92330546+Toodoo-BlastOff@users.noreply.github.com>
2022-05-02 14:07:08 -0300

    Upload do script SQL para os alunos fazerem a engenharia reversa

---
+++ Banco de Dados/Tarefa3Modelamento.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS exercicio;
+CREATE DATABASE exercicio;
+USE exercicio;
+
+CREATE TABLE IF NOT EXISTS curso(
+	id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(10),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS aluno(
+	matricula INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(20),
+    ano_nasc int,
+    sexo ENUM('F', 'M'),
+    fk_idcurso INT,
+    CONSTRAINT fk
+		FOREIGN KEY (fk_idcurso) REFERENCES curso(id) ON UPDATE CASCADE 
+);
